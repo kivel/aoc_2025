976a6a7904e0db3308cef0d2c540396b3717b9ce
Niko Kivel <niko.kivel@lightsource.ca>
2025-12-04 13:32:32 -0600

    use filter on positions directly

--- src/bin/day04_1.rs
+++ src/bin/day04_1.rs
@@ -1,6 +1,11 @@
 #[path = "../advent_of_code/mod.rs"]
 mod advent_of_code;
 
+fn is_accessible(grid: &advent_of_code::Grid, row: usize, col: usize) -> bool {
+    let surrounding_chars = grid.get_surrounding_chars(row, col);
+    let at_count = surrounding_chars.iter().filter(|&&c| c == '@').count();
+    at_count < 4
+}
 // The puzlle calls for two lists (given as two columns in a ascii file) to be sorted and line by line the absolute differences need to be summed up.
 fn puzzle(data: &Vec<String>) -> u32 {
     // lines_to_matrix, aka grid
@@ -8,30 +13,17 @@ fn puzzle(data: &Vec<String>) -> u32 {
     // find all '@' positions
     let at_positions = grid.find_char_positions('@');
     println!("Found <{}> '@'", at_positions.len());
-    // go over all '@' positions and check if they are accessible (less than 4 surrounding '@')
-    let mut accessible_count = 0;
-    for (row, col) in at_positions {
-        let surrounding_chars = grid.get_surrounding_chars(row, col);
-        let at_count = surrounding_chars.iter().filter(|&&c| c == '@').count();
-        if at_count < 4 {
-            accessible_count += 1;
-        }
-    }
+    // filter positions directly
+    let accessible_count = at_positions.iter()
+        .filter(|&&(row, col)| is_accessible(&grid, row, col))
+        .count();
+    
     println!("Accessible '@' count: {}", accessible_count);
     accessible_count as u32
 }
 
 fn main() {
     let d = advent_of_code::Reader::read_file("./input/day04_test.txt").unwrap();
-    // let grid = advent_of_code::Grid::from_lines(&d);
-    // grid.println();
-    // let positions = grid.find_char_positions('@');
-    // println!("Positions of '@': {:?}", positions);
-    // let first_position = positions[0];
-    // let chars_around = grid.get_surrounding_chars(first_position.0, first_position.1);
-    // let is_accessible = chars_around.iter().filter(|c| **c == '@').count() < 4;
-    // println!("Chars around first '@': {:?}", chars_around);
-    // println!("Is first '@' accessible? {}", is_accessible);
     let sum = puzzle(&d);
     println!("result: {sum}");
 }
