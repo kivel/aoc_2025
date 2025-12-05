f8b84a1403531b7b23679ee88666dec8d110f770
Niko Kivel <niko.kivel@lightsource.ca>
2025-12-04 19:34:09 -0600

    profiling

--- src/bin/day04_2.rs
+++ src/bin/day04_2.rs
@@ -30,6 +30,7 @@ fn is_accessible(grid: &advent_of_code::Grid, row: usize, col: usize, remaining_
 
 // The puzlle calls for two lists (given as two columns in a ascii file) to be sorted and line by line the absolute differences need to be summed up.
 fn puzzle(data: &Vec<String>) -> u32 {
+    let start_time = std::time::Instant::now();
     // lines_to_matrix, aka grid
     let grid = advent_of_code::Grid::from_lines(&data);
     // find all '@' positions
@@ -59,11 +60,14 @@ fn puzzle(data: &Vec<String>) -> u32 {
     
     let final_count = accessible_count;
     println!("Remaining '@' count: {}", final_count);
+    let stop_time = std::time::Instant::now();
+    let duration = stop_time.duration_since(start_time);
+    println!("Duration: {:?}", duration);
     final_count as u32
 }
 
 fn main() {
-    let d = advent_of_code::Reader::read_file("./input/day04_test.txt").unwrap();
+    let d = advent_of_code::Reader::read_file("./input/day04.txt").unwrap();
     let sum = puzzle(&d);
     println!("result: {sum}");
 }
